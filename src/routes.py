487c3039b167830a97bc400bada3473431726dd1
guga neri <guganeriss@gmail.com>
2020-06-27 22:21:06 -0300

    finalizando routes

--- src/routes.py
+++ src/routes.py
@@ -25,16 +25,29 @@ def home():
     return "API-CAT", 200
 
 
-@app.route('/listar-racas', methods=['GET'])
+@app.route('/listar-racas/', methods=['GET'])
 def listar_racas():      
     cats = dumps(catCollection.find())
     response = Response(status=200, headers={}, response=cats, mimetype='application/json')
     return response
 
 
-@app.route('/listar-racas/<string:raca>/', methods=['GET'])     
+@app.route('/listar-racas/raca/<string:raca>/', methods=['GET'])     
 def listar_racas_inf(raca):
     cats = dumps(catCollection.find({"id": raca}))
+    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
+    return response
+
+@app.route('/listar-racas/temperamento/<string:temperamento>/', methods=['GET'])
+def listar_racas_temperamento(temperamento):
+    cats = dumps(catCollection.find({"temperamen": {"$regex": temperamento}}))
+    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
+    return response
+
+
+@app.route('/listar-racas/origem/<string:origem>/', methods=['GET'])
+def listar_racas_origin(origem):
+    cats = dumps(catCollection.find({"origin": origem}))
     response = Response(status=200, headers={}, response=cats, mimetype='application/json')
     return response
 
