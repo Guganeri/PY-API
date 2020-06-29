24588d1cfef16555e22701f68624c13e5f85afe9
guga neri <guganeriss@gmail.com>
2020-06-29 17:30:22 -0300

    iniciando logs

--- src/routes.py
+++ src/routes.py
@@ -4,6 +4,7 @@ from bson.json_util import dumps
 import json
 import pymongo
 import os
+import logging
 
 load_dotenv()
 BDURL = os.getenv("BDURL")
@@ -14,61 +15,82 @@ catCollection = db['api_cat']
 hatCatCollection = db['hat_Cat']
 glassCatCollection = db['glass_Cat']
  
+### Logs inf ###
+logging.basicConfig(filename='meow.log', level=logging.DEBUG)
 
+
+### API ROUTES INICIO ###
 app = Flask("API-CAT")
 
 ### API_CAT ###
 @app.route('/', methods=['GET'])
 def home():    
-    return "API-CAT", 200
-
+    return "API-CAT", 200   
 
 ### Listar todas as raças ###
 @app.route('/listar-racas/', methods=['GET'])
 def listar_racas():      
-    cats = dumps(catCollection.find())
-    if (list(cats) == []):
+    cats = list(catCollection.find())    
+    if (cats == []):
         response = Response(status=404)
+        return response
     else:
-        response = Response(status=200, headers={}, response=cats, mimetype='application/json')
+        response = Response(status=200, headers={}, response=dumps(cats), mimetype='application/json')
         return response
 
 ### Listar informações partindo de uma raça ###
 @app.route('/listar-racas/<string:raca>/', methods=['GET'])     
 def listar_racas_inf(raca):
-    cats = dumps(catCollection.find({"id": raca}))
-    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
-    return response
+    cats = list(catCollection.find({"id": raca}))
+    if (cats == []):
+        response = Response(status=404)
+        return response
+    else:
+        response = Response(status=200, headers={}, response=dumps(cats), mimetype='application/json')
+        return response
 
 ### Listar raças partindo de um temperamento ###
 @app.route('/listar-racas/temperamento/<string:temperamento>/', methods=['GET'])
 def listar_racas_temperamento(temperamento):
-    cats = dumps(catCollection.find({"temperament": {"$regex": temperamento}}))
-    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
-    return response
+    cats = list(catCollection.find({"temperament": {"$regex": temperamento}}))
+    if (cats == []):
+        response = Response(status=404)
+        return response
+    else:
+        response = Response(status=200, headers={}, response=dumps(cats), mimetype='application/json')
+        return response
 
 ### Listar raças partindo de uma origem ### 
 @app.route('/listar-racas/origem/<string:origem>/', methods=['GET'])
 def listar_racas_origin(origem):
-    cats = dumps(catCollection.find({"origin": origem}))
-    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
-    return response
+    cats = list(catCollection.find({"origin": origem}))
+    if (cats == []):        
+        response = Response(status=404)
+        return response
+    else:
+        response = Response(status=200, headers={}, response=dumps(cats), mimetype='application/json')
+        return response
 
 ### Listar imagem de gatos com chapéu ###
 @app.route('/listar/img/chapeu', methods=['GET'])
 def listar_img_chapeu():
-    cats = dumps(hatCatCollection.find())
-    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
-    return response
+    cats = list(hatCatCollection.find())
+    if (cats == []):
+        response = Response(status=404)
+        return response
+    else:
+        response = Response(status=200, headers={}, response=dumps(cats), mimetype='application/json')
+        return response
 
 ### Listar imagem de gatos com óculus ###
 @app.route('/listar/img/oculus', methods=['GET'])
 def listar_img_oculus():
-    cats = dumps(glassCatCollection.find())
-    response = Response(status=200, headers={}, response=cats, mimetype='application/json')
-    return response 
-
-
+    cats = list(glassCatCollection.find())
+    if (cats == []):
+        response = Response(status=404)
+    else:
+        response = Response(status=200, headers={}, response=dumps(cats), mimetype='application/json')
+        return response 
   
 if __name__ == '__main__':
     app.run(debug=False)
